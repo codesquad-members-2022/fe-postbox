34744fb3ba3adb9bf051ec4e04cd521ce633778a
SeungHyun <fm10033@gmail.com>
2022-03-08 15:34:20 +0900

    FEAT: renderTown()정해진  규칙에 맞게 랜더링
    층마다 boorder 색상, size 정함

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
+const $townMap = document.querySelector("#town-map");
+
+const LAYER_SIZE = {
+  0: 300,
+  1: 100,
+  2: 30,
+  3: 10,
+};
+
+const LAYER_COLOR = {
+  0: "red",
+  1: "blue",
+  2: "green",
+  3: "tomato",
+};
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
 }
 
-function renderTown(layer) {
+function renderChild($child, $parentNode, layer) {
+  $child.style.width = `${LAYER_SIZE[layer]}px`;
+  $child.style.height = `${LAYER_SIZE[layer]}px`;
+  $child.style.border = `1px solid ${LAYER_COLOR[layer]}`;
+  $child.classList.add("town");
+  $parentNode.appendChild($child);
+}
+
+function renderTown($parentNode, layer) {
   if (layer > 3) {
     return;
   }
   const townNumber = getRandomNumber(0, 2);
   for (let i = 0; i < townNumber + 1; i++) {
     console.log(`${layer}층: ${i}`);
+    const $child = document.createElement("div");
+    renderChild($child, $parentNode, layer);
+
     // 자식을 렌더링하는 함수
     // 렌더링할 마을 개수: 0~2
-    renderTown(layer + 1);
+    renderTown($child, layer + 1);
   }
 }
 
 function init() {
-  renderTown(0);
+  renderTown($townMap, 0);
 }
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
