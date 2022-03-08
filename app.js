951f61aa504dea9a86b9d796680834b4ff5413d7
JinJeon <nanyong01@gmail.com>
2022-03-08 11:35:41 +0900

    feat : 마을 안에 자식 마을이 들어가고, 자식 마을이 부모 마을을 벗어나지 않도록 기능 추가

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const vilages = [];
-const vilageTemplate = () => {
+const villages = [];
+
+const villageTemplate = () => {
   return document.createElement("div");
 };
 
@@ -11,17 +12,36 @@ const randomLocation = (max = 1000, min = 10) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-for (let i = 0; i < 3; i++) {
-  const vilage = vilageTemplate();
-  vilage.style.width = `${randomLength()}px`;
-  vilage.style.height = `${randomLength()}px`;
-  vilage.style.top = `${randomLocation()}px`;
-  vilage.style.left = `${randomLocation()}px`;
-  vilage.style.position = "relative";
-  vilage.style.border = "2px solid";
-  vilages.push(vilage);
-}
+const village = villageTemplate();
+const width = randomLength();
+const height = randomLength();
 
+village.style.width = `${width}px`;
+village.style.height = `${height}px`;
+village.style.top = `${randomLocation()}px`;
+village.style.left = `${randomLocation()}px`;
+village.style.position = "relative";
+village.style.border = "2px solid";
+
+const getInnerVillage = (width, height) => {
+  const innerVillage = villageTemplate();
+  const innerWidth = randomLength(width - 10);
+  const innerHeight = randomLength(height - 10);
+  const innerTop = randomLocation(height - innerHeight - 3);
+  const innerLeft = randomLocation(width - innerWidth - 3);
+
+  innerVillage.style.width = `${innerWidth}px`;
+  innerVillage.style.height = `${innerHeight}px`;
+  innerVillage.style.border = "2px solid";
+  innerVillage.style.top = `${innerTop}px`;
+  innerVillage.style.left = `${innerLeft}px`;
+  innerVillage.style.position = "relative";
+
+  return innerVillage;
+};
+
+const childVillage = getInnerVillage(width, height);
 const body = document.querySelector("body");
-vilages.forEach((vilage) => body.append(vilage));
-console.log(vilages);
+
+village.append(childVillage);
+body.append(village);
