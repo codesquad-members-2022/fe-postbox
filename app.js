104cdd272bd554183334f39ab84f73de13fae9cd
nanjeong <whdwkxj7@naver.com>
2022-03-08 12:10:29 +0900

    feat : 여러 개의 마을이 중첩되도록 하는 함수 추가

--- app.js
+++ app.js
@@ -12,23 +12,12 @@ const randomLocation = (max = 1000, min = 10) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-const village = villageTemplate();
-const width = randomLength();
-const height = randomLength();
-
-village.style.width = `${width}px`;
-village.style.height = `${height}px`;
-village.style.top = `${randomLocation()}px`;
-village.style.left = `${randomLocation()}px`;
-village.style.position = "relative";
-village.style.border = "2px solid";
-
 const getInnerVillage = (width, height) => {
   const innerVillage = villageTemplate();
-  const innerWidth = randomLength(width - 10);
-  const innerHeight = randomLength(height - 10);
-  const innerTop = randomLocation(height - innerHeight - 3);
-  const innerLeft = randomLocation(width - innerWidth - 3);
+  const innerWidth = randomLength(width - 1);
+  const innerHeight = randomLength(height - 1);
+  const innerTop = randomLocation(height - innerHeight - 1);
+  const innerLeft = randomLocation(width - innerWidth - 1);
 
   innerVillage.style.width = `${innerWidth}px`;
   innerVillage.style.height = `${innerHeight}px`;
@@ -40,8 +29,38 @@ const getInnerVillage = (width, height) => {
   return innerVillage;
 };
 
-const childVillage = getInnerVillage(width, height);
+const range = (number) => {
+  return [...Array(number)].map((_, index) => index);
+};
+
+const getVillageChunk = (number) => {
+  const outerVillage = villageTemplate();
+  let village = outerVillage;
+  let width = randomLength();
+  let height = randomLength();
+  const top = randomLocation();
+  const left = randomLocation();
+
+  village.style.width = `${width}px`;
+  village.style.height = `${height}px`;
+  village.style.top = `${top}px`;
+  village.style.left = `${left}px`;
+  village.style.position = "relative";
+  village.style.border = "2px solid";
+
+  range(number).forEach((_) => {
+    const innerVillage = getInnerVillage(width, height);
+    village.append(innerVillage);
+
+    width = innerVillage.style.width.slice(0, -2);
+    height = innerVillage.style.height.slice(0, -2);
+    village = innerVillage;
+  });
+
+  return outerVillage;
+};
+
+const vilageChunk = getVillageChunk(2);
 const body = document.querySelector("body");
 
-village.append(childVillage);
-body.append(village);
+body.append(vilageChunk);
