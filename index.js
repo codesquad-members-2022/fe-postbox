3a75209c9c3826671a3d0fc99384966f0bc4aa10
yunho <kimyouknow@naver.com>
2022-03-08 16:53:10 +0900

    Refactor: layer-size랜덤으로 생성 및 위치부여 함수 분리

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ const FLEX_DIRECTION = {
 };
 
 const LAYER_SIZE = {
-  0: 300,
-  1: 100,
-  2: 30,
-  3: 10,
+  0: getRandomNumber(280, 320),
+  1: getRandomNumber(80, 200),
+  2: getRandomNumber(40, 60),
+  3: getRandomNumber(10, 15),
 };
 
 const LAYER_COLOR = {
@@ -38,14 +38,18 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
 }
 
+function getLocation($element) {
+  $element.style.justifyContent = JUSTIFY_CONTENT[getRandomNumber(0, 3)];
+  $element.style.alignItems = ALIGN_ITEMS[getRandomNumber(0, 3)];
+  $element.style.flexDirection = FLEX_DIRECTION[getRandomNumber(0, 1)];
+}
+
 function renderChild($child, $parentNode, layer) {
   $child.style.width = `${LAYER_SIZE[layer]}px`;
   $child.style.height = `${LAYER_SIZE[layer]}px`;
+  // $child.style.margin = '5%';
   $child.style.border = `1px solid ${LAYER_COLOR[layer]}`;
   $child.classList.add('town');
-  $child.style.justifyContent = JUSTIFY_CONTENT[getRandomNumber(0, 3)];
-  $child.style.alignItems = ALIGN_ITEMS[getRandomNumber(0, 3)];
-  $child.style.flexDirection = FLEX_DIRECTION[getRandomNumber(0, 1)];
   $parentNode.appendChild($child);
 }
 
@@ -53,6 +57,7 @@ function renderTown($parentNode, layer) {
   if (layer > 3) {
     return;
   }
+  getLocation($parentNode);
   const townNumber = getRandomNumber(0, 2);
   for (let i = 0; i < townNumber + 1; i++) {
     console.log(`${layer}층: ${i}`);
