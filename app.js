01a8cb782d37bc90fdbd76b66f16941f00cab48c
JinJeon <nanyong01@gmail.com>
2022-03-08 15:44:40 +0900

    fix : 자식 마을이 부모 마을을 벗어나지 않도록 수정함

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
 const villages = [];
+const BORDER = 2;
+const LENGTH_MIN = 50;
+const LENGTH_MAX = 500;
+const DISTANCE_MIN = 20;
 
 const villageTemplate = () => {
   return document.createElement("div");
 };
 
-const randomLength = (max = 400, min = 100) => {
-  return Math.floor(Math.random() * (max - min) + min);
+const randomLength = (range) => {
+  return Math.floor(Math.random() * (range.max - range.min) + range.min);
 };
 
-const randomLocation = (max = 1000, min = 10) => {
-  return Math.floor(Math.random() * (max - min) + min);
+const randomLocation = (range) => {
+  return Math.floor(Math.random() * (range.max - range.min) + range.min);
 };
 
 const getInnerVillage = (width, height) => {
   const innerVillage = villageTemplate();
-  const innerWidth = randomLength(width - 1);
-  const innerHeight = randomLength(height - 1);
-  const innerTop = randomLocation(height - innerHeight - 1);
-  const innerLeft = randomLocation(width - innerWidth - 1);
+  const innerHeight = randomLength({
+    min: LENGTH_MIN,
+    max: height - 2 * BORDER - DISTANCE_MIN,
+  });
+  const innerWidth = randomLength({
+    min: LENGTH_MIN,
+    max: width - 2 * BORDER - DISTANCE_MIN,
+  });
+  const innerTop = randomLocation({
+    min: DISTANCE_MIN,
+    max: height - 2 * BORDER - innerHeight,
+  });
+  const innerLeft = randomLocation({
+    min: DISTANCE_MIN,
+    max: width - 2 * BORDER - innerWidth,
+  });
+
+  if (innerHeight < LENGTH_MIN || innerWidth < LENGTH_MIN) return null;
 
   innerVillage.style.width = `${innerWidth}px`;
   innerVillage.style.height = `${innerHeight}px`;
-  innerVillage.style.border = "2px solid";
+  innerVillage.style.border = `${BORDER}px solid`;
   innerVillage.style.top = `${innerTop}px`;
   innerVillage.style.left = `${innerLeft}px`;
   innerVillage.style.position = "relative";
@@ -36,25 +54,33 @@ const range = (number) => {
 const getVillageChunk = (number) => {
   const outerVillage = villageTemplate();
   let village = outerVillage;
-  let width = randomLength();
-  let height = randomLength();
-  const top = randomLocation();
-  const left = randomLocation();
+  let width = randomLength({ min: LENGTH_MIN, max: LENGTH_MAX });
+  let height = randomLength({ min: LENGTH_MIN, max: LENGTH_MAX });
+  const left = randomLocation({
+    min: DISTANCE_MIN,
+    max: screen.availWidth - width,
+  });
+  const top = randomLocation({
+    min: DISTANCE_MIN,
+    max: screen.availHeight - height,
+  });
 
   village.style.width = `${width}px`;
   village.style.height = `${height}px`;
   village.style.top = `${top}px`;
   village.style.left = `${left}px`;
   village.style.position = "relative";
-  village.style.border = "2px solid";
+  village.style.border = `${BORDER}px solid`;
 
   range(number).forEach((_) => {
     const innerVillage = getInnerVillage(width, height);
-    village.append(innerVillage);
+    if (innerVillage) {
+      village.append(innerVillage);
 
-    width = innerVillage.style.width.slice(0, -2);
-    height = innerVillage.style.height.slice(0, -2);
-    village = innerVillage;
+      width = innerVillage.style.width.slice(0, -2);
+      height = innerVillage.style.height.slice(0, -2);
+      village = innerVillage;
+    }
   });
 
   return outerVillage;
