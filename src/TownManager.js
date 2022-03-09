b41f7bd1b54b682875954e43205d748b0e51180c
bangdler <zbthz90@gmail.com>
2022-03-09 21:06:32 +0900

    feat: create get element by className

--- src/TownManager.js
+++ src/TownManager.js
@@ -5,12 +5,8 @@ import { getRandomNumber } from "./utils.js";
 class TownManager {
   constructor() {
     this.towns = [];
-    // TODO: min, max 모듈화시킬지 고민.
-    this.townNumber = getRandomNumber({ min: 1, max: 10 });
-    this.postOfficeNumber = getRandomNumber({
-      min: 1,
-      max: this.townNumber - 1,
-    });
+
+    console.log(document.querySelector('.name'))
   }
 
   makeTowns() {
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,3 @@
-import { MAP_SIZE } from "./constants.js";
 
 function getRandomNumber({ min, max }) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,6 +9,19 @@ function getDatasetNames(nodes) {
     names = [...names, node.dataset.name];
   });
   return names;
+}
+
+function getElementByClassName(className) {
+  const stack = [document];
+  while(stack.length) {
+    const curEl = stack.pop();
+    if(curEl.className === className) {
+      return curEl
+    }
+    if(curEl.childNodes) {
+      stack.push(...curEl.childNodes);
+    }
+  }
 }
 
 export { getRandomNumber, getDatasetNames };
