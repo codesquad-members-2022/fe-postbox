dde5f01a7cd72579b362000c17ccb718f31941e0
Cola <bcad1591@gmail.com>
2022-03-09 15:18:12 +0900

    Feat: DOM 탐색 모듈 추가
    getElementByClassName, getElementsByClassName 두개 추가

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,10 @@
-export const selector = (selectorName, base = document) => {
-  return base.querySelector(selectorName);
-};
-
-export const selectorAll = (selectorName, base = document) => {
-  return base.querySelectorAll(selectorName);
-};
-
 export const range = (from, to) => {
   const diff = to - from;
   return (Math.random() * diff + from) >> 0;
   // from ~ to-1
 };
+
+export {
+  getElementByClassName,
+  getElementsByClassName,
+} from '../dom/getElement.js';
