05f675ea3f84415435527234133a4001956310e7
Cola <bcad1591@gmail.com>
2022-03-09 11:17:02 +0900

    Feat: 랜덤한 범위의 숫자를 반환하는 range함수 추가

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,13 @@
 export const selector = (selectorName, base = document) => {
   return base.querySelector(selectorName);
 };
+
 export const selectorAll = (selectorName, base = document) => {
   return base.querySelectorAll(selectorName);
+};
+
+export const range = (from, to) => {
+  const diff = to - from;
+  return (Math.random() * diff + from) >> 0;
+  // from ~ to-1
 };
