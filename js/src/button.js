e112b53d155169c8d054e020c846f415fbe7cbee
seungchanHan <seungchanhan00@gmail.com>
2022-03-10 16:40:27 +0900

    [ADD] 버튼 클릭시 이벤트 완성

--- js/src/button.js
+++ js/src/button.js
@@ -5,27 +5,56 @@ export default class Button {
   btnAddEvent() {
     this.redButton.addEventListener('click', this.startEvent);
   }
-  startEvent = ({ target }) => {
-    const villageList = document.querySelectorAll('.village');
-    const mailbox = [...villageList].filter(
-      (village) => village.lastElementChild.className === 'postbox'
-    );
-    const villageTotal = document.querySelector('.village-total');
-    const postboxSize = document.querySelector('.postbox-size');
-    const postboxInfo = mailbox.map((element) => {
+
+  startEvent = () => {
+    const $postbox = document.querySelectorAll('.postbox');
+
+    const postboxInfo = this.getPostboxInfo($postbox);
+
+    this.showPostbox(postboxInfo);
+    this.showSortedPostbox(postboxInfo);
+
+    setTimeout(() => {
+      this.changeBorder($postbox);
+    }, 2000);
+  };
+
+  getPostboxInfo($postbox) {
+    return [...$postbox].map((element) => {
       return {
-        name: element.firstElementChild.innerHTML,
-        size: element.lastElementChild.style.padding,
+        name: element.previousElementSibling.innerHTML,
+        size: element.style.width,
       };
     });
-    // console.log(postboxInfo);
+  }
+
+  changeBorder($postbox) {
+    $postbox.forEach(
+      (element) => (element.parentNode.style.border = '2px solid red')
+    );
+  }
+
+  showSortedPostbox(postboxInfo) {
+    const $postboxSize = document.querySelector('.postbox-size');
+    let showSortedPostbox = '우체통의 크기는 <br>';
+    const sorted = this.getSort(postboxInfo);
+    sorted.forEach(
+      (element) => (showSortedPostbox += ` ${element.name}(${element.size})`)
+    );
+    showSortedPostbox += '<br>순서 입니다.';
+    $postboxSize.innerHTML = showSortedPostbox;
+  }
+
+  showPostbox(postboxInfo) {
+    const $villageTotal = document.querySelector('.village-total');
     let showPostbox = '';
+
     postboxInfo.forEach((element) => (showPostbox += ` ${element.name}`));
+
     showPostbox += `<br> 총 ${postboxInfo.length}개의 마을입니다.`;
-    showPostbox += villageTotal.innerHTML = showPostbox;
-    // console.log(this);
-    console.log(this.getSort(postboxInfo));
-  };
+    $villageTotal.innerHTML = showPostbox;
+  }
+
   getSort(postboxInfo) {
     return [...postboxInfo].sort(
       (a, b) => b.size.substr(0, 1) - a.size.substr(0, 1)
