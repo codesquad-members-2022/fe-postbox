b91af568a9b49ee80a1b8092ea2ccf6d5bfb8616
yongseongjeon <yongseong.jeon95@gmail.com>
2022-03-10 17:22:35 +0900

    chore: modify fetch url

--- public/src/index.js
+++ public/src/index.js
@@ -4,8 +4,8 @@ import { addCheckBtnEvent } from "./event.js";
 async function init() {
   sizeMap();
 
-  const towns = await fetch("http://localhost:3000/towns").then((res) =>
-    res.json()
+  const towns = await fetch("https://random-towns.herokuapp.com/towns").then(
+    (res) => res.json()
   );
 
   towns.forEach(renderTown);
