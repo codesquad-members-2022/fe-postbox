dfa6bdbb9f400b7150e23041e2060a3544b98507
sju02048 <sju02048@naver.com>
2022-03-10 18:56:43 +0900

    Feat: 랜덤값 만드는 함수 추가

--- public/js/util/util.js
+++ public/js/util/util.js
@@ -1,1 +1,10 @@
 export const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const getRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+export const convertStringToHTML = (string) => {
+  const div = document.createElement("div");
+  div.innerHTML = string;
+  return div.firstElementChild;
+};
