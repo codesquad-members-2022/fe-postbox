3f8165496143fc8bf015e253a6a6277557351841
yongseongjeon <yongseong.jeon95@gmail.com>
2022-03-10 15:47:22 +0900

    chore: install and set nodemon

--- public/src/server.js
+++ public/src/server.js
@@ -1,18 +1,17 @@
-
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.listen(PORT, () => {
-    console.log('listening')
-})
+  console.log(`✅ listening on http://localhost:${PORT}/`);
+});
 
-app.use(express.static(path.join(__dirname, '..')))
+app.use(express.static(path.join(__dirname, "..")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "index.html"));
+});
