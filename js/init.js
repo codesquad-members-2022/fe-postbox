581989e571f5d1828af24a60d7a1dbd8d31708f3
seungchanHan <seungchanhan00@gmail.com>
2022-03-10 16:38:01 +0900

    [MOD] 간격 띄어쓰기

--- js/init.js
+++ js/init.js
@@ -1,13 +1,18 @@
 import Model from './src/model.js';
 import View from './src/view.js';
 import Button from './src/button.js';
+
 const $ = (select) => document.querySelector(select);
 
 (function startSearchVillage() {
   const villageInfo = $('.village-info');
+  const redButton = $('.red-button');
+
   const model = new Model();
+
   const view = new View(model, villageInfo);
   view.renderVillage();
+
   const button = new Button(redButton);
   button.btnAddEvent();
 })();
