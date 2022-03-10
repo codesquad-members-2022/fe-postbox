edd6932fec1a7281bb31625dcdd39c612675b5f7
seungchanHan <seungchanhan00@gmail.com>
2022-03-10 16:38:50 +0900

    [ADD] 마을 안에 마을 추가

--- js/src/view.js
+++ js/src/view.js
@@ -2,14 +2,35 @@ export default class View {
   constructor(model, villageInfo) {
     this.model = model;
     this.villageInfo = villageInfo;
-    this.renderVillage();
   }
   renderVillage() {
     const villageData = this.model.getVillage();
-    let template = '';
-    for (let i = 0; i < villageData.length; i++) {
-      template += `<div class = 'village' style = 'width:${villageData[i].width}px; height:${villageData[i].height}px;'>${villageData[i].name}</div>`;
-    }
-    this.villageInfo.innerHTML = template;
+
+    const addTemplate = (village) => {
+      let template = `<div class = 'village' style = 'width:${
+        village.width
+      }px; height:${village.height}px;'>
+            <span>${village.name}</span>
+            ${
+              village.postbox.exist
+                ? `<span class = 'postbox' style = 'width : ${village.postbox.size}px;'>📮</span>`
+                : ''
+            }
+            
+            `;
+      if (village.child.length) {
+        village.child.forEach((element) => (template += addTemplate(element)));
+      }
+      template += `</div>`;
+      return template;
+    };
+
+    let villageTemplate = '';
+
+    villageData.forEach((element) => {
+      villageTemplate += addTemplate(element);
+    });
+    console.log(villageData);
+    this.villageInfo.innerHTML = villageTemplate;
   }
 }
