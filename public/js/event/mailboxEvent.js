18b4db6824b2b77a50204947c8f575f4c10e419c
sju02048 <sju02048@naver.com>
2022-03-11 16:31:03 +0900

    Feat: 퀵소트 정렬 추가

--- public/js/event/mailboxEvent.js
+++ public/js/event/mailboxEvent.js
@@ -2,29 +2,40 @@ import {
   getElementByClassName,
   getElementsByClassName,
 } from "../util/dom-lib.js";
-import { delay } from "../util/util.js";
+import { delay, quickSort } from "../util/util.js";
 import { renderMailboxInfo } from "../render.js";
 
 export const mailboxEventHandler = () => {
+  console.time("시작");
   let towns = [];
   const $towns = getElementsByClassName("town");
+
+  Array.prototype.quickSort = function () {
+    return quickSort.call(undefined, this);
+  };
+
   const sortedTowns = $towns
     .filter((town) =>
       [...town.children[0].children].some((child) =>
         child.classList.contains("mailbox")
       )
-    )
+    ) // FIXME
     .map((town) => {
-      delay(2000).then(() => town.classList.add("has-mailbox"));
+      delay(1000).then(() => {
+        town.classList.add("has-mailbox");
+        console.timeEnd("시작");
+      });
       towns.push(getElementByClassName("town-title", town).innerText);
-      return town;
+
+      return {
+        name: getElementByClassName("town-title", town).innerText,
+        mailboxSize: Number(
+          getElementByClassName("mailbox", town).dataset.size
+        ),
+      };
     })
-    .sort(
-      (townA, townB) =>
-        Number(getElementByClassName("mailbox", townB).dataset.size) -
-        Number(getElementByClassName("mailbox", townA).dataset.size)
-    )
-    .map((town) => getElementByClassName("town-title", town).innerText);
+    .quickSort()
+    .map((town) => town.name);
 
   renderMailboxInfo(towns, sortedTowns);
 };
--- public/js/util/util.js
+++ public/js/util/util.js
@@ -8,3 +8,20 @@ export const convertStringToHTML = (string) => {
   div.innerHTML = string;
   return div.firstElementChild;
 };
+export const quickSort = (list) => {
+  if (list.length <= 1) return list;
+
+  const pivot = list[Math.floor(list.length / 2)].mailboxSize;
+  const left = [],
+    equal = [],
+    right = [];
+
+  for (const town of list) {
+    const size = town.mailboxSize;
+    if (size < pivot) right.push(town);
+    else if (size > pivot) left.push(town);
+    else equal.push(town);
+  }
+
+  return [...quickSort(left), ...equal, ...quickSort(right)];
+};
