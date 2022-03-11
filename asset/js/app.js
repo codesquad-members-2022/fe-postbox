9fa6a5e40df01b20bc42091b01e85ee60c8d3dbf
otterp012 <87624756+otterp012@users.noreply.github.com>
2022-03-11 19:58:22 +0900

    [otter & J] Step2 (#24)

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,4 +1,4 @@
-class Selectors {
+class Utils {
   constructor() {}
 
   static IDSelector(str, start = document.body) {
@@ -7,8 +7,8 @@ class Selectors {
     visited[start] = true;
     while (queue.length) {
       for (let i = 0; i < queue.length; i++) {
-        let parent = queue.shift();
-        for (let child of parent.childNodes) {
+        const parent = queue.shift();
+        for (const child of parent.childNodes) {
           if (child.id == str) return child;
           else {
             visited[child] = true;
@@ -17,9 +17,10 @@ class Selectors {
         }
       }
     }
+    return null;
   }
 
-  static AllclassSelector(str) {
+  static allClassSelector(str) {
     const start = document.body;
     const queue = [start];
     const visited = {};
@@ -28,9 +29,9 @@ class Selectors {
 
     while (queue.length) {
       for (let i = 0; i < queue.length; i++) {
-        let parent = queue.shift();
-        for (let child of parent.childNodes) {
-          if (String(child.className).indexOf(str) !== -1) result.push(child);
+        const parent = queue.shift();
+        for (const child of parent.childNodes) {
+          if (String(child.className).includes(str)) result.push(child);
           else {
             visited[child] = true;
             queue.push(child);
@@ -49,9 +50,9 @@ class Selectors {
 
     while (queue.length) {
       for (let i = 0; i < queue.length; i++) {
-        let parent = queue.shift();
-        for (let child of parent.childNodes) {
-          if (String(child.className).indexOf(str) !== -1) return child;
+        const parent = queue.shift();
+        for (const child of parent.childNodes) {
+          if (String(child.className).includes(str)) return child;
           else {
             visited[child] = true;
             queue.push(child);
@@ -59,13 +60,43 @@ class Selectors {
         }
       }
     }
+    return null;
+  }
+
+  static mergeSort(arr) {
+    const len = arr.length;
+    if (len == 1) {
+      return arr;
+    }
+    const middle = Math.floor(len / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle, len);
+    function merge(left, right) {
+      const result = [];
+      while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+          result.push(left.shift());
+        } else {
+          result.push(right.shift());
+        }
+      }
+      while (left.length) {
+        result.push(left.shift());
+      }
+      while (right.length) {
+        result.push(right.shift());
+      }
+      return result;
+    }
+    return merge(this.mergeSort(left), this.mergeSort(right));
   }
 }
 
 class TownGenerator {
-  constructor(maxTownCount, bigTownCount) {
+  constructor(maxTownCount, bigTownCount, postBoxCount) {
     this.maxTownCount = maxTownCount;
     this.bigTownCount = bigTownCount;
+    this.postBoxCount = postBoxCount;
     this.count = null;
     this.set = null;
     this.init();
@@ -74,7 +105,7 @@ class TownGenerator {
   init() {
     this.renderBigTown();
     this.renderSmallTown();
-    this.renderPost(4);
+    this.renderPost(this.postBoxCount);
   }
 
   getRandomInt(min, max) {
@@ -92,17 +123,17 @@ class TownGenerator {
       html += this.renderTown(i, 'bigTowns');
     }
 
-    Selectors.classSelector('town-container').innerHTML = html;
-    Selectors.AllclassSelector('bigTowns').forEach((bigTown) => {
+    Utils.classSelector('town-container').innerHTML = html;
+    Utils.allClassSelector('bigTowns').forEach((bigTown) => {
       bigTown.style.placeSelf = this.getLocation();
     });
   }
 
   renderSmallTown() {
-    while (this.count <= this.maxTownCount) {
+    while (this.count < this.maxTownCount) {
       let rand = this.getRandomInt(1, this.count);
       this.count++;
-      Selectors.IDSelector(rand).innerHTML += this.renderTown(this.count);
+      Utils.IDSelector(rand).innerHTML += this.renderTown(this.count);
     }
   }
 
@@ -131,8 +162,12 @@ class TownGenerator {
   getPostCount(number) {
     const set = new Set();
     for (let i = 0; i < number; i++) {
-      const randomId = this.getRandomInt(1, this.maxTownCount);
-      const postSize = i;
+      let randomId = this.getRandomInt(1, this.maxTownCount);
+      let postSize = this.getRandomInt(1, this.maxTownCount);
+      set.forEach((arr) => {
+        if (arr[0] === randomId) randomId += 1;
+        if (arr[1] === postSize) postSize += 1;
+      });
       set.add([randomId, postSize]);
     }
     return set;
@@ -145,13 +180,83 @@ class TownGenerator {
   renderPost(number) {
     this.set = this.getPostCount(number);
     this.set.forEach((arr) => {
-      Selectors.IDSelector(arr[0]).insertAdjacentText('afterbegin', '📮 ');
+      Utils.IDSelector(arr[0]).insertAdjacentText('afterbegin', '📮 ');
     });
   }
 }
 
+class EventListener {
+  constructor(town) {
+    this.town = town;
+    this.sortedInfor = null;
+    this.btnClickEventHandler();
+  }
+
+  refineInfor() {
+    const postInfo = this.town.set;
+    let townSorted = [];
+    postInfo.forEach((townID) => {
+      townSorted.push(townID[0]);
+    });
+    townSorted = Utils.mergeSort(townSorted);
+
+    const temp = {};
+    [...postInfo].forEach((town) => {
+      temp[town[1]] = town[0];
+    });
+    const postSorted = Object.values(temp);
+
+    this.sortedInfor = {
+      townSorted,
+      postSorted,
+    };
+  }
+
+  convertTownBorderColor(ms) {
+    this.delay(ms).then(() =>
+      this.sortedInfor.townSorted.forEach((townID) => {
+        Utils.IDSelector(townID).style.borderColor = 'red';
+      })
+    );
+  }
+
+  delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  btnClickEventHandler() {
+    document.body.addEventListener('click', ({ target }) => {
+      switch (target.className) {
+        case 'postbox-check-btn':
+          this.refineInfor();
+          this.convertTownBorderColor(2000);
+          this.printText(this.sortedInfor);
+          break;
+        case 'reload-btn':
+          this.reload();
+          break;
+      }
+    });
+  }
+
+  reload() {
+    location.reload();
+  }
+
+  printText(obj) {
+    Utils.classSelector(
+      'infor-text'
+    ).innerHTML = `우체통을 가지고 있는 마을은 ${obj.townSorted}입니다.
+    <br>
+    <br>
+    우체통 크기의 순서는 ${obj.postSorted}입니다.`;
+  }
+}
+
 (function () {
-  const townMaxCount = 10;
+  const townMaxCount = 20;
   const bigTownCount = 4;
-  const town = new TownGenerator(townMaxCount, bigTownCount);
+  const postBoxCount = 4;
+  const town = new TownGenerator(townMaxCount, bigTownCount, postBoxCount);
+  const eventListener = new EventListener(town);
 })();
