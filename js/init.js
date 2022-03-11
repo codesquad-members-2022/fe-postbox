852c8ea204343af2bc2dc477b2db472b11f5d7f4
seungchanHan <seungchanhan00@gmail.com>
2022-03-11 15:54:44 +0900

    [MOD] 불필요한 querySelector 제거

--- js/init.js
+++ js/init.js
@@ -1,17 +1,13 @@
 import Model from './src/model.js';
 import View from './src/view.js';
 import Button from './src/button.js';
-import { myQuerySelector } from './utility/querySelector.js';
 
 (function startSearchVillage() {
-  const villageInfo = myQuerySelector(document.body, 'village-info');
-  const redButton = myQuerySelector(document.body, 'red-button');
-
   const model = new Model();
 
-  const view = new View(model, villageInfo);
+  const view = new View(model);
   view.renderVillage();
 
-  const button = new Button(redButton);
+  const button = new Button();
   button.btnAddEvent();
 })();
