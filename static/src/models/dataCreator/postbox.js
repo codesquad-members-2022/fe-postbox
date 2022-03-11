e92ad271d39bc8cfbe6c6e29109b9595bbc6eeb8
dony <rsefa94@gmail.com>
2022-03-12 00:54:02 +0900

    Feat: postbox 랜덤 데이터 생성 구현

--- static/src/models/dataCreator/postbox.js
+++ static/src/models/dataCreator/postbox.js
@@ -6,18 +6,31 @@ const getPostboxNumber = () => {
   return postboxNumber;
 };
 
-const getPostboxLength = (townDataNumber) => {
-  const postboxLengthList = new Array(townDataNumber).map(() => {
+const getPostboxLength = (postboxNumber) => {
+  const postboxLengthList = new Array(postboxNumber).fill(0).map(() => {
     const postboxLength = getRandomNumber(POSTBOX_RANGE.length.min, POSTBOX_RANGE.length.max);
     return postboxLength;
   });
   return postboxLengthList;
 };
 
+const getPostboxTownIndice = (postboxNumber, townNumber) => {
+  const postboxTownIndice = new Set();
+  while (postboxTownIndice.size < postboxNumber) {
+    const postboxTownIndex = getRandomNumber(0, townNumber - 1);
+    postboxTownIndice.add(postboxTownIndex);
+  }
+  return [...postboxTownIndice];
+};
+
 export const updatePostboxNumber = (postboxData) => {
   postboxData.number = getPostboxNumber();
 };
 
-export const updatePostboxLength = (townData) => {
-  townData.length = getPostboxLength(townData.number);
+export const updatePostboxLength = (postboxData) => {
+  postboxData.length = getPostboxLength(postboxData.number);
+};
+
+export const updatePostboxTownIndice = (postboxData, townDataNumber) => {
+  postboxData.postboxTownIndex = getPostboxTownIndice(postboxData.number, townDataNumber);
 };
