72255415884da7f1a602ea00d4e80cb4d559d213
soralee <sora2821@gmail.com>
2022-03-11 15:45:31 +0900

    Feat: postbox, town range constant 추가

---
+++ static/src/constant.js
@@ -0,0 +1,24 @@
+export const POSTBOX_RANGE = {
+  number: {
+    min: 1,
+    max: 8,
+  },
+  length: {
+    min: 3,
+    max: 10,
+  },
+};
+
+export const TOWN_RANGE = {
+  number: {
+    max: 26,
+  },
+  coordinate: {
+    min: 0,
+    max: 80,
+  },
+  length: {
+    min: 20,
+    max: 100,
+  },
+};
