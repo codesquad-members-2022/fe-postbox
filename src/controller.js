bf126b54fbd6f0e9dd80e59d3be32640a22b4290
jm <jm.dev.study@gmail.com>
2022-03-11 15:01:02 +0900

    [feat] controller.js: ‘빨간 우체통 확인’ 버튼 클릭시 이벤트 추가 - 우체통을 가진 마을은 2초 뒤에 빨간색 테두리로 표시된다. - 우체통을 가진 마을 정보와 우체통 크기 정렬을 텍스트로 화면에 출력한다. - 아직 정렬방식은 따로 구현하지 않았고 sort를 사용했다.

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,6 @@
 import { Map } from './model.js';
 import { View } from './view.js';
+import { TraverseDOM } from './util.js';
 
 class Controller {
   constructor(view, model) {
@@ -9,7 +10,35 @@ class Controller {
 
   init() {
     this.model.randomCreateTown();
-    this.view.renderArea(this.model.townList);
+    this.view.renderRootTown(this.model.townList);
+    this.buttonClickEventHandler();
+  }
+
+  buttonClickEventHandler() {
+    const postBoxBtn = TraverseDOM.querySelector(document, 'info__btn');
+    const findPostBox = TraverseDOM.querySelectorAll(document, 'postbox');
+    const infoTown = TraverseDOM.querySelector(document, 'info__town');
+    const infoPostBox = TraverseDOM.querySelector(document, 'info__postbox');
+
+    const postBoxInfo = [];
+    postBoxBtn.addEventListener('click', () => {
+      findPostBox.forEach((box) => {
+        this.view.changeBorderColor(box);
+        if (!infoTown.innerText) {
+          postBoxInfo.push({ name: box.parentNode.firstChild.innerText, size: box.dataset.postboxSize });
+        }
+      });
+
+      const postBoxNames = postBoxInfo.map((box) => box.name).join(', ');
+      this.view.addPostBoxTownText(infoTown, postBoxNames, postBoxInfo);
+
+      // 임시로 sort 사용
+      const sortResult = postBoxInfo
+        .sort((a, b) => a.size - b.size)
+        .map((box) => box.name)
+        .join(', ');
+      this.view.addPostBoxSortText(infoPostBox, sortResult);
+    });
   }
 }
 
